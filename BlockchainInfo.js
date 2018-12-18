e70b6d1963623250723fcfe4a855a82101e56361
Gayathridevi <gayathri.suresh@gmail.com>
2018-12-18 08:48:12 -0800

    Added git ignore file

--- BlockchainInfo.js
+++ BlockchainInfo.js
@@ -1,8 +1,8 @@
 var Params = {
-    PROVIDER_NODE: '',   //Insert Network URL
+    PROVIDER_NODE: 'https://ropsten.infura.io/v3/95dae3f6bf554b21ac29fbf9bce02426',   //Insert Network URL
     CHAIN_ID: 3,         //Replace Network Id
-    ETHER_ACC : '',      //This account should have an Ether balance
-    PVT_KEY : '',        //The account's private key
+    ETHER_ACC : '0xF774d13c30C7a979898577Fb7946f1Dfa289906A',      //This account should have an Ether balance
+    PVT_KEY : 'cb4f3e8a8fa7b8b4c08fd18853f4216b9e20b51d01ce6403dfb396dc86484051',        //The account's private key
     GAS_LIMIT : 7500000  //Set Gas limit
 };
 
