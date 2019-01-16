5e5a96c744a75eec5115d705b27728c2f1e04792
amodi01 <amitmodi.79@gmail.com>
2019-01-16 15:27:48 +0530

    setting blockchain info and truffle to ropsten network

--- BlockchainInfo.js
+++ BlockchainInfo.js
@@ -1,8 +1,8 @@
 var Params = {
-    PROVIDER_NODE: '',   //Insert Network URL
+    PROVIDER_NODE: 'https://ropsten.infura.io/v3/e24aadd136f442c4a0a7f589ab4b8764',   //Insert Network URL
     CHAIN_ID: 3,         //Replace Network Id
-    ETHER_ACC : '',      //This account should have an Ether balance
-    PVT_KEY : '',        //The account's private key
+    ETHER_ACC : '0x6828e3a6B9649d112bE2948a0B098Ee55d4527fB',  //0xdE7bba95a05B47004F90a2a0BB2055ADE28D64Eb    //This account should have an Ether balance
+    PVT_KEY :  '83971f10d60836b7a48bc4a92349c27100c8d5391ec86339c3a386b01cb6bbf4',   // 1f1704fecc0f04354aece6dddd23741b8b88e45746dd0c1d00a6dbdff8a12711     //The account's private key
     GAS_LIMIT : 7500000  //Set Gas limit
 };
 
--- truffle.js
+++ truffle.js
@@ -12,12 +12,50 @@
  *   },
  */
 
+require('babel-register');
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+  rinkeby:{
+        provider: function() {
+          return new HDWalletProvider( "calm combine hip snow evil cement add type dust palace dune oxygen", "https://rinkeby.infura.io/v3/e24aadd136f442c4a0a7f589ab4b8764");
+        },
+        network_id:4,
+        gas : 6721975,
+        gasPrice: 100000000000,
+        from: '0x6828e3a6B9649d112bE2948a0B098Ee55d4527fB'
+      },
+  ropsten:{
+    provider: function() {
+      return new HDWalletProvider( "calm combine hip snow evil cement add type dust palace dune oxygen", "https://ropsten.infura.io/v3/e24aadd136f442c4a0a7f589ab4b8764");
+    },
+    network_id:3,
+    gas : 6721975,
+    gasPrice: 100000000000,
+    from: '0x6828e3a6B9649d112bE2948a0B098Ee55d4527fB'
+  }
+},
+  solc: {
+    version: "0.4.24",
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
+
+// module.exports = {
+//   networks: {
+//     development: {
+//       host: "127.0.0.1",
+//       port: 8545,
+//       network_id: "*" // Match any network id
+//     }
+//   }
+// };
